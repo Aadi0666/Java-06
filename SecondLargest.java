9295b11faf93e0ba44b5509ca1fe9b14f46fdde8
Aadi0666 <adityatyagi0110@gmail.com>
2025-08-27 12:00:14 +0530

    Slargest and ssmallest element in array

---
+++ SecondLargest.java
@@ -0,0 +1,26 @@
+public class SecondLargest {
+    public static void main(String[] args) {
+        int[] arr = { 1, 5, 7, 7, 15, 9 };
+        int largest = arr[0];
+        int slargest = -1;
+        int smallest = Integer.MAX_VALUE;
+        int ssmallest = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                slargest = largest;
+                largest = arr[i];
+            } else if (arr[i] < largest && arr[i] > slargest) {
+                slargest = arr[i];
+            }
+            if (arr[i] < smallest) {
+                ssmallest = smallest;
+                smallest = arr[i];
+            } else if (arr[i] < ssmallest && arr[i] > smallest) {
+                ssmallest = arr[i];
+            }
+        }
+        System.out.println("SecondLargest element is: " + slargest);
+        System.out.println("Secondsmallest element is: " + ssmallest);
+    }
+}
